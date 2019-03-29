8bad432298fb906da8e25149c413837c75ab0dce
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2019-03-28 19:10:08 -0600

    se agregan varios procesos funcionales y enviar archivos grandes

--- lib/utils.c
+++ lib/utils.c
@@ -1,55 +1,71 @@
+//
+// Created by emmanuel on 26/03/19.
+//
+
 #include "utils.h"
 
 void disable_buffers(){
-	setbuf(stdout,NULL);
+    setbuf(stdout,NULL);
 }
+
+
 
 void exit_on_error(const char *error){
-	printf("[ERROR] %s \n", error);
-	print_error_status();
-	printf(">>> Program finished.\n");
-	exit(1);
+    printf("[ERROR] %s \n", error);
+    print_error_status();
+    printf(">>> Program finished.\n");
+    exit(1);
 }
 
+
+
 void print_error_status(){
-	printf("Error status: %s\n", strerror(errno));
+    printf("Error status: %s\n", strerror(errno));
 }
+
+
 
 int create_socket(){
 
-	int socket_fd;
+    int socket_fd;
 
     // Create the socket
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if ( socket_fd  == ERROR) { 
-       	exit_on_error("Error creating the socket");
-    } 
+    if ( socket_fd  == ERROR) {
+        exit_on_error("Error creating the socket");
+    }
+    return socket_fd;
 }
 
+
+
 void bind_socket(int socket_fd, int port){
-	struct sockaddr_in address; 
-    address.sin_family = AF_INET; 
-    address.sin_port = htons( port ); 
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons( port );
     address.sin_addr.s_addr = INADDR_ANY;
 
-    int address_length = sizeof(address);      
+    int address_length = sizeof(address);
 
     // Bind the socket to the port
     int result = bind(socket_fd, (struct sockaddr *) &address, address_length );
-    if(result == ERROR ){ 
-    	close(socket_fd);
-    	exit_on_error("Error binding the socket to port");
-    } 
+    if(result == ERROR ){
+        close(socket_fd);
+        exit_on_error("Error binding the socket to port");
+    }
 }
+
 
 
 void start_listening(int socket_fd, int max_clients){
-	int result = listen(socket_fd, max_clients);
-    if (result == ERROR){ 
-    	close(socket_fd);
+    int result = listen(socket_fd, max_clients);
+    if (result == ERROR){
+        close(socket_fd);
         exit_on_error("Error starting to listen");
-    } 
+    }
 }
+
+
 
 void connect_to_server(int socket_fd, int port){
     //Specify an address for the socket
@@ -62,68 +78,78 @@ void connect_to_server(int socket_fd, int port){
     int connection_status = connect(socket_fd, (struct sockaddr *)&client_address, sizeof(client_address));
 
     if(connection_status == -1){
-    	close(socket_fd);
+        close(socket_fd);
         exit_on_error("Error connecting to server");
     }
 }
 
 
+
 void parse_http_request(char *string, http_request *request){
-	char method[20], uri[256], protocol[20];
-	sscanf(string,"%s %s %s", method, uri, protocol); 
-	printf("-method:%s\n", method);
+    char method[20], uri[256], protocol[20];
+    sscanf(string,"%s %s %s", method, uri, protocol);
+    printf("-method:%s\n", method);
     printf("-uri:%s\n", uri);
     printf("-protocol:%s\n", protocol);
 
-	if( strcmp(protocol,"HTTP/1.1") != EQUALS)
-		exit_on_error("Unknown HTTP protocol");
-	
-	if(strcmp(method,"GET"))
-		request->method = GET;
-	else if (strcmp(method,"POST"))
-		request->method = POST;
-	else if (strcmp(method,"DELETE"))
-		request->method = DELETE;
-	else if (strcmp(method,"PUT"))
-		request->method = PUT;
-	else
-		exit_on_error("Unknown HTTP method");
-	
-	strcpy(request->uri, uri);
-	strcpy(request->protocol, protocol);
+    if( strcmp(protocol,"HTTP/1.1") != EQUALS)
+        exit_on_error("Unknown HTTP protocol");
+
+    if(strcmp(method,"GET"))
+        request->method = GET;
+    else if (strcmp(method,"POST"))
+        request->method = POST;
+    else if (strcmp(method,"DELETE"))
+        request->method = DELETE;
+    else if (strcmp(method,"PUT"))
+        request->method = PUT;
+    else
+        exit_on_error("Unknown HTTP method");
+
+    strcpy(request->uri, uri);
+    strcpy(request->protocol, protocol);
 
 }
+
+
 
 void parse_http_response(char *string, http_response *response){
-	return;
+    return;
 }
 
+
+
 void build_filename(char *folder, char *uri, char *dest){
-    // Add the final "/" to the folder name 
+    // Add the final "/" to the folder name
     int offset = sprintf(dest,"%s/",folder);
     // +1 to skip the starting "/" of the uri
     offset += sprintf(dest+offset,"%s",uri+1);
 
 }
 
-void send_response(int socket_fd, http_response response){
+
+
+// Sends all the header of the reponse the caller must send the file
+void send_response_header(int socket_fd, http_response response){
+    char *phrases[600];
+    phrases[404] = "Not Found";
+    phrases[200] = "OK";
+
+    char buffer[1024];
     // All the response will be written here
-    char buffer[10*1024];
-    char *protocol = "HTTP/1.1";
-    // This is used to track where to write the next time 
+    const char *protocol = "HTTP/1.1";
+    // This is used to track where to write the next time
     int offset = sprintf(buffer,"%s %d %s\n",protocol,response.status_code,phrases[response.status_code]);
 
     if(response.status_code == 200){
-        offset += sprintf(buffer+offset,"Content-length: %d\n",response.content_length);
+        offset += sprintf(buffer+offset,"Content-length: %ld\n",response.content_length);
         offset += sprintf(buffer+offset,"\n");
-        //Write exactly the size of the body 
-        memcpy(buffer+offset, response.body, response.content_length);
-        offset += response.content_length;
     }
 
-    int bytes_sent = send(socket_fd, buffer, offset,0);  
-    printf("%d bytes sent content: %s\n",bytes_sent,buffer);
+    int bytes_sent = send(socket_fd, buffer, offset,0);
 }
+
+
 
 int copy_file(FILE *file, char *buffer){
     int bytes_count = 0;
@@ -133,17 +159,21 @@ int copy_file(FILE *file, char *buffer){
     return bytes_count;
 }
 
+
+
 void fill_phrases(){
-    phrases[404] = "Not Found";
-    phrases[200] = "OK";
+    // phrases[404] = "Not Found";
+    // phrases[200] = "OK";
 }
+
+
 
 void parse_arguments(int argc, char *argv[], arguments *arguments){
     if(argc != 7){
         exit_on_error("Invalid amount of arguments");
     }
 
-    char current_option_char; 
+    char current_option_char;
     while((current_option_char = getopt(argc, argv, "n:w:p:")) != -1){
         switch(current_option_char){
             case 'n':
@@ -156,5 +186,41 @@ void parse_arguments(int argc, char *argv[], arguments *arguments){
                 sscanf(optarg,"%d",&arguments->port);
                 break;
         }
+    }
+}
+
+
+
+long int get_file_size(const char *file_name){
+    long int size = -1;
+    struct stat st; /*declare stat variable*/
+    /*get the size using stat()*/
+    if(stat(file_name,&st)==0)
+        return size = st.st_size;
+    return size;
+}
+
+
+
+char *get_shared_memory_segment(int size, key_t key){
+    int PERMISSIONS = 0666;
+    int segment_id = shmget(key, size, IPC_CREAT | PERMISSIONS);
+
+    if(segment_id == -1)
+        exit_on_error("error creating segment");
+
+    char *shared_memory = shmat(segment_id, NULL, 0);
+
+    if(shared_memory == (char*)-1)
+        exit_on_error("error attaching");
+    *shared_memory = 'k';
+    return shared_memory;
+}
+
+
+
+void fill_array(int *array, int n, int value){
+    for (int i = 0; i < n; ++i) {
+        *(array+i) = value;
     }
 }
--- preforked_server/server.c
+++ preforked_server/server.c
@@ -6,8 +6,65 @@
 #define REQUEST_BUFFER_SIZE 1*KB
 #define FILE_BUFFER_SIZE 10*MB
 #define CHILD 0
-#define NO_FREE_WORKERS -1 
+#define NO_FREE_WORKERS -1
 
+// COMMAND TO REQUEST clear ; cat port.txt | xargs -I % curl http://localhost:%/a.html
+
+/*
+    Read the request from the socket and responds it based on the received relative path
+    Params:
+        client_socket: where the function will read the request and write the response
+        path: any resource requested will be searched within this path
+ */
+void handle_client(int client_socket, char *path){
+
+    // Read the filename
+    char request_buffer[1024];
+
+    read( client_socket, request_buffer, sizeof(request_buffer));
+    http_request req;
+
+    parse_http_request(request_buffer, &req);
+
+    char filename[20];
+
+    build_filename(path,req.uri, filename);
+
+    FILE *file = fopen(filename, "r");
+
+    http_response response;
+    if(file){
+        long int file_size = get_file_size(filename);
+        response.content_length = file_size;
+        response.status_code = 200;
+
+        send_response_header(client_socket, response);
+
+        // Write the file to the socket by parts using a buffer
+        char buff[1024];
+        int bytes_read;
+        while(file_size){
+            bytes_read = fread(buff, 1, sizeof(buff), file);
+            send(client_socket, buff, sizeof(buff),0);
+            file_size -= bytes_read;
+        }
+        fclose(file);
+    }
+    else{
+        printf("File not found\n");
+        response.status_code = 404;
+        printf("sending response...");
+        send_response_header(client_socket, response);
+        printf("sending response done\n");
+    }
+
+}
+
+
+
+/*
+    Passes the socket descriptor from parent process to a worker so it can serve the request
+ */
 ssize_t sock_fd_write(int sock, void *buf, ssize_t buflen, int fd){
     ssize_t     size;
     struct msghdr   msg;
@@ -35,12 +92,12 @@ ssize_t sock_fd_write(int sock, void *buf, ssize_t buflen, int fd){
         cmsg->cmsg_level = SOL_SOCKET;
         cmsg->cmsg_type = SCM_RIGHTS;
 
-        printf ("passing fd %d\n", fd);
+//        printf ("passing fd %d\n", fd);
         *((int *) CMSG_DATA(cmsg)) = fd;
     } else {
         msg.msg_control = NULL;
         msg.msg_controllen = 0;
-        printf ("not passing fd\n");
+//        printf ("not passing fd\n");
     }
 
     size = sendmsg(sock, &msg, 0);
@@ -52,6 +109,9 @@ ssize_t sock_fd_write(int sock, void *buf, ssize_t buflen, int fd){
 
 
 
+/*
+    The worker reads the file descriptor of the socket by this function
+ */
 ssize_t sock_fd_read(int sock, void *buf, ssize_t bufsize, int *fd){
     ssize_t     size;
 
@@ -82,17 +142,17 @@ ssize_t sock_fd_read(int sock, void *buf, ssize_t bufsize, int *fd){
         if (cmsg && cmsg->cmsg_len == CMSG_LEN(sizeof(int))) {
             if (cmsg->cmsg_level != SOL_SOCKET) {
                 fprintf (stderr, "invalid cmsg_level %d\n",
-                     cmsg->cmsg_level);
+                         cmsg->cmsg_level);
                 exit(1);
             }
             if (cmsg->cmsg_type != SCM_RIGHTS) {
                 fprintf (stderr, "invalid cmsg_type %d\n",
-                     cmsg->cmsg_type);
+                         cmsg->cmsg_type);
                 exit(1);
             }
 
             *fd = *((int *) CMSG_DATA(cmsg));
-            printf ("received fd %d\n", *fd);
+//            printf ("received fd %d\n", *fd);
         } else
             *fd = -1;
     } else {
@@ -107,73 +167,80 @@ ssize_t sock_fd_read(int sock, void *buf, ssize_t bufsize, int *fd){
 
 
 
-void handle_client(int client_socket, char *path){
+/*
+   Reads the key to access shared memory received from the parent process from the socket
+   and return the value
+ */
+int receive_key(int parent_socket_fd){
+    char buffer[20];
+    recv(parent_socket_fd, buffer, sizeof(buffer), 0);
+    // printf("key: %s\n",buffer);
+    int key;
+    sscanf(buffer,"%d",&key);
+    return key;
 
-    // Read the filename
-    char request_buffer[1024];
-
-    read( client_socket, request_buffer, sizeof(request_buffer));
-    http_request req;
-    
-    parse_http_request(request_buffer, &req);
-
-    char filename[20];
-
-    build_filename(path,req.uri, filename);
-
-    FILE *file = fopen(filename, "r");
-
-    http_response response;
-    char file_buffer[15*1024];
-    if(file){
-        int file_size = copy_file(file, file_buffer);
-        response.content_length = file_size;
-        response.body = file_buffer;
-        response.status_code = 200;
-        fclose(file);
-    }
-    else{
-        printf("File not found\n");
-        response.status_code = 404;
-    }
-    printf("sending response...\n"); 
-    send_response(client_socket, response);
-    printf("sending response done\n");
 }
 
 
 
+/*
+    Defines the behavior of the workers made with the fork() call
+ */
 void child_role(int parent_socket_fd){
+    // Get the key to access shared memory from parent process
+    int key = receive_key(parent_socket_fd);
+
+    // Get the shared memory key from parent
+    char *mem = get_shared_memory_segment(1,key);
+
     // Get the socket fd from parent
     char buf[16];
     int client_socket_fd = -1;
     int pid = getpid();
-    printf("Child with pid %d waiting for work\n",pid);
-    int size = 0;
-    while(! size){
-        sleep(0.5);
-        size = sock_fd_read(parent_socket_fd, buf, sizeof(buf), &client_socket_fd);
+
+    while(1) {
+        *mem = '1'; // Report available
+        printf("Child with pid %d waiting for work\n",pid);
+        int size = 0;
+        while (!size) {
+            sleep(0.5);
+            size = sock_fd_read(parent_socket_fd, buf, sizeof(buf), &client_socket_fd);
+        }
+        *mem = '0'; // Report busy
+        printf("Child with pid %d received work: %d\n", pid, client_socket_fd);
+        if (size <= 0) {
+            printf("Child read nothing\n");
+        }
+        // printf("read %d\n", (int) size);
+        if (client_socket_fd != -1) {
+            // Attend the request
+            printf("Child with pid %d entering handle_client\n", pid);
+            handle_client(client_socket_fd, "www");
+            printf("Child with pid %d done handle_client\n", pid);
+            close(client_socket_fd);
+        }
     }
-    printf("Child with pid %d received work\n",pid);
-    if (size <= 0)
-        printf("Child read nothing\n");
-    printf ("read %d\n", (int)size);
-    if (client_socket_fd != -1) {
-        // Attend the request 
-        printf("Child with pid %d entering handle_client\n",pid);
-        handle_client(client_socket_fd,"www");
-        printf("Child with pid %d done handle_client\n",pid);
-        // close(client_socket_fd);
-        // close(parent_socket_fd);
-    }
-    printf("Child with pid: %d finished\n",pid);
-    exit(0);
 }
 
 
 
-void init_workers(int n, int *sockets_to_childs){
+
+/*
+    Creates n processes (workers) that runs child_role() function, creates the pointers to the shared
+    memory that will hold the states of the workers (saved to char* array) and the keys to access the
+    shared memory are saved to the int array
+    Params:
+        n: the number of workers to create
+        sockets_to_childs: an array of ints where the socket descriptors to the workers will be saved
+        child_states: an array of char* that hold the state of the workers
+        keys: the array where the keys to access the shared memory will be saved
+ */
+void init_workers(int n, int *sockets_to_childs, char *childs_states[], int *keys){
     for (int i = 0; i < n; ++i){
+        int key = 9990+i;
+        childs_states[i] = get_shared_memory_segment(1, key);
+        *(childs_states[i]) = '0';
+        *(keys+i) = key;
         int socket_fds[2];
         if (socketpair(AF_LOCAL, SOCK_STREAM, 0, socket_fds) < 0) {
             exit_on_error("Error creating socket pair");
@@ -196,24 +263,40 @@ void init_workers(int n, int *sockets_to_childs){
 
 
 
-// Returns the index of a free worker of the array
-int get_worker(int *sockets_to_childs, int *free_index, int max_workers){
-    int result = NO_FREE_WORKERS;
-    if(*(free_index) < max_workers){
-        *(free_index)++;
-        result = *(free_index);
+
+/*
+    Returns the index of a free worker of the array
+    If there's no one available then returns NO_FREE_WORKER
+ */
+int get_worker(char *workers_states[], int max_workers){
+    int index = NO_FREE_WORKERS;
+    int found = 0; // Boolean flag
+    for (int i = 0 ; i < max_workers && ! found ; ++i) {
+        if (*(workers_states[i]) == '1') {
+            index = i;
+            found = 1;
+
+        }
     }
-    return result;
+    return index;
 }
 
 
 
+/*
+    Sends an "Server full" html document to the client received
+ */
 int respond_server_full(int client_socket_fd){
-    // build an mock html and send repsonde+}
+    // build an mock html and send repsonde
+    return 0;
 }
 
 
 
+
+/*
+    Prints an array of ints
+ */
 void print_array(int *array, int size){
     printf("[");
     int i;
@@ -225,60 +308,114 @@ void print_array(int *array, int size){
 
 
 
+
+/*
+    Prints an array of char* (the char pointed by the pointer)
+ */
+void print_char_array(char **array, int size){
+    printf("[");
+    int i;
+    for (i = 0; i < size-1; ++i){
+        printf("%c, ",*(array[i]));
+    }
+    printf("%c]\n",*(array[i]));
+}
+
+
+
+/*
+    Writes port to a port.txt file in current directory
+    THIS IS TEMPORARILY FOR TESTING
+ */
+void write_to_file(int port){
+    FILE *f = fopen("port.txt","w");
+    fprintf(f,"%d",port);
+    fclose(f);
+}
+
+
+
+
+/*
+    The main procedure of the Prefork HTTP Server
+ */
 int main(int argc, char *argv[]) {
 
+    // Parse arguments received from terminal
     arguments args;
     parse_arguments(argc,argv,&args);
-
     disable_buffers();
 
+    // Make the map of status_code-to-phrase of http codes
     fill_phrases();
 
-	int socket_fd = create_socket();
+    //Create the socket for receiving HTTP requests
+    int socket_fd = create_socket();
 
     bind_socket(socket_fd, args.port);
 
     start_listening(socket_fd, MAX_QUEUE_LENGTH);
 
-    int served_clients=0, fails=0;
+    //TEMP
+    write_to_file(args.port);
 
-    // This will hold the socket pairs to communicate 
-    // parent process to its childs
+
+    // Because some data structures have the same size as the number of processes to use
     int n = args.processes;
-    int sockets_to_childs[n];
-    init_workers(n, sockets_to_childs);
-    int free_worker_index = 0;
-    struct sockaddr_in client_address;
-    while(served_clients < n){
-        printf("Waiting clients at %d | served: %d received: %d\n",args.port,served_clients, fails);
-    	int client_socket_fd = accept(socket_fd, 
-    		(struct sockaddr *) &client_address, 
-    		(socklen_t*) &client_address);
 
-    	if(client_socket_fd == ERROR){
-    		printf("Error accepting client\n");
-            print_error_status();
-            fails++;
-    	}
+    // This will hold the sockets to communicate this parent process to its children (workers)
+    int sockets_to_workers[n];
 
-    	else{
+    // This array has the pointers to the byte that says the state of the worker {'1'=available '0'=busy}
+    char *workers_states[n];
+
+    // This holds the keys to access the memory (state byte) shared with the worker
+    int keys[n];
+
+    // Make the forks, get the shared bytes, and get the keys to share memory with the workers
+    init_workers(n, sockets_to_workers, workers_states, keys);
+
+    // See keys
+    // print_array(keys,n);
+
+    // See states
+    // print_char_array(workers_states, n);
+
+    // Send the key to each worker so it can report its state
+    char b[10];
+    for (int i = 0; i <n ; ++i) {
+        sprintf(b,"%d",keys[i]);
+        send(sockets_to_workers[i], b, sizeof(b),0);
+    }
+
+
+    while(1){
+
+        printf("Waiting clients at %d \n",args.port);
+        // printf("Before accept "); print_char_array(workers_states, n); printf("\n");
+        int client_socket_fd = accept(socket_fd, NULL, NULL);
+
+        if(client_socket_fd == ERROR){
+            exit_on_error("Error accepting client\n");
+        }
+        else{
             // Check if there's free workers
-            int worker = get_worker(sockets_to_childs, &free_worker_index, n);
-            if(worker == NO_FREE_WORKERS){
+            int worker_index = get_worker(workers_states, n);
+            if(worker_index == NO_FREE_WORKERS){
+
                 // If not then respond with "full" message
                 printf("Server is full\n");
                 respond_server_full(client_socket_fd);
             }
             else{
-                // If there is then send the client socket to 
-                // the worker so it can handle the request
-                sock_fd_write(worker, "1", 1, client_socket_fd);
+                 // If there is then send the client socket to the worker so it can handle the request
+                 sock_fd_write(sockets_to_workers[worker_index], "1", 1, client_socket_fd);
             }
-            served_clients++;
-            // Back to accept
-    	}
+            // printf("After sending work: "); print_char_array(workers_states, n); printf("\n");
+            //Back to accept
+        }
     }
-	
-	close(socket_fd);    
-    return 0; 
- }
+
+    close(socket_fd);
+    return 0;
+}
