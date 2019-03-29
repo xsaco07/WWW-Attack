665a5d90237b2aef4e188cc63b13e016c05b0210
xsaco07 <xsaco07@gmail.com>
2019-03-28 23:02:47 -0600

    Se rechazan clientes despues del limite con un mensaje html

--- prethread_server/server_pre_thread.c
+++ prethread_server/server_pre_thread.c
@@ -6,18 +6,23 @@ void *handle_client();
 _Bool arguments_OK(int argc, char* argv[]);
 void initiate_threads();
 void save_arguments(char* argv[]);
-void answering_client(int client_socket_fd, char buffer[]);
+void answering_client(int client_socket_fd, char buffer[], int limit_reached);
 void show_threads_use();
+void create_rejecting_clients_thread();
+void* answering_when_limit_reached();
 
 int NEW_CLIENT_SOCKET_FD = -1;
 int MAIN_SOCKET_ID = -1;
 int MAX_CLIENTS = 20;
+
+char limit_reached_filename[32] = "/NO_MORE_CLIENTS.html";
 
 pthread_mutex_t available_threads_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t new_client_fd_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_cond_t new_client_condition = PTHREAD_COND_INITIALIZER;
 pthread_cond_t available_threads_condition = PTHREAD_COND_INITIALIZER;
+pthread_cond_t not_available_threads_condition = PTHREAD_COND_INITIALIZER;
 
 int main(int argc, char *argv[]) {
 
@@ -27,9 +32,11 @@ int main(int argc, char *argv[]) {
     save_arguments(argv);
     create_thread_pool(PROGRAM.NUM_CLIENT_THREADS);
 
+    create_rejecting_clients_thread();
+
     MAIN_SOCKET_ID = create_socket();
     bind_socket(MAIN_SOCKET_ID, PROGRAM.ARGUMENT_PORT);
-    start_listening(MAIN_SOCKET_ID, MAX_CLIENTS);
+    start_listening(MAIN_SOCKET_ID, PROGRAM.NUM_CLIENT_THREADS);
     initiate_threads();
     handle_conexion();
     close(MAIN_SOCKET_ID);
@@ -47,6 +54,11 @@ void show_threads_use(){
   PROGRAM.AVAILABLE_THREADS, PROGRAM.NUM_CLIENT_THREADS - PROGRAM.AVAILABLE_THREADS);
 }
 
+void create_rejecting_clients_thread(){
+  pthread_t thread;
+  pthread_create(&thread, NULL, answering_when_limit_reached, NULL);
+}
+
 void handle_conexion(){
 
   while (1) {
@@ -61,33 +73,64 @@ void handle_conexion(){
     }
     else{
       printf("\nConexion established\n");
+
+      // START CRITIC REGION -----------------------------------------------------
       // Update the socket fd that the threads are reading from.
       pthread_mutex_lock(&new_client_fd_mutex);
       NEW_CLIENT_SOCKET_FD = client_socket_fd;
       pthread_mutex_unlock(&new_client_fd_mutex);
-
+      // END CRITIC REGION -----------------------------------------------------
       printf("\nThe client fd is = %d\n", NEW_CLIENT_SOCKET_FD);
 
       pthread_cond_broadcast(&new_client_condition); // Unlock all threads locked in this condition
 
+      // START CRITIC REGION ---------------------------------------------------
       pthread_mutex_lock(&available_threads_mutex);
       PROGRAM.AVAILABLE_THREADS--;
 
       if (PROGRAM.AVAILABLE_THREADS == 0) {
+        printf("\nSERVER HAS REACHED THE LIMIT OF CONCURRENT CLIENTS\n");
         pthread_cond_wait(&available_threads_condition, &available_threads_mutex);
       }
 
       pthread_mutex_unlock(&available_threads_mutex);
+      // END CRITIC REGION -----------------------------------------------------
     }
 
   }
   pthread_exit(NULL);
 }
 
-void* handle_client(){
+void* answering_when_limit_reached(){
+  while (1) {
+    if(!PROGRAM.AVAILABLE_THREADS){
+
+      printf("\nIm listening for clients who can not be attended.\n");
+      int client_socket_fd = accept(MAIN_SOCKET_ID, NULL, NULL);
+
+      if(client_socket_fd == ERROR){
+        printf("Error accepting new client\n");
+        print_error_status();
+      }
+      else{
+        printf("\nConexion established\n");
+        printf("\nThe client fd is = %d\n", client_socket_fd);
+        printf("\nClient can not be attended.\nThere is no space.\n");
+
+        char buffer[BUFFER_SIZE];
+        recv(client_socket_fd, buffer, BUFFER_SIZE, 0);
+        answering_client(client_socket_fd, buffer, 1);
+        close(client_socket_fd);
+      }
+    }
+  }
+}
+
+void* handle_client(void* limit_reached){
 
   int client_socket_fd;
   char buffer[BUFFER_SIZE];
+  int limit_flag = *(int*)limit_reached;
 
   while (1) {
 
@@ -113,7 +156,7 @@ void* handle_client(){
       printf("\nReading from client %d\n", client_socket_fd);
 
       recv(client_socket_fd, buffer, BUFFER_SIZE, 0);
-      answering_client(client_socket_fd, buffer);
+      answering_client(client_socket_fd, buffer, limit_flag);
       close(client_socket_fd);
 
       printf("\nClient %d attended succesfully\n", client_socket_fd);
@@ -132,13 +175,14 @@ void* handle_client(){
   pthread_exit(NULL);
 }
 
-void answering_client(int client_socket_fd, char buffer[]){
+void answering_client(int client_socket_fd, char buffer[], int limit_reached){
 
   http_request req;
   parse_http_request(buffer, &req);
 
   char filename[20];
-  build_filename(PROGRAM.PATH_ROOT,req.uri, filename);
+  if(!limit_reached) build_filename(PROGRAM.PATH_ROOT,req.uri, filename);
+  else build_filename(PROGRAM.PATH_ROOT,limit_reached_filename, filename);
 
   printf("\nFile asked = %s\n", filename);
 
@@ -171,7 +215,8 @@ _Bool arguments_OK(int argc, char* argv[]){
 
 void initiate_threads(){
   for (int i = 0; i < PROGRAM.NUM_CLIENT_THREADS; i++) {
-    if(pthread_create(&(thread_pool[i]), NULL, handle_client, NULL)){
+    int initial_limit_flag = 0;
+    if(pthread_create(&(thread_pool[i]), NULL, handle_client, (void*)&initial_limit_flag)){
       printf("\nError in creation of the thread #%d\n", i);
       print_error_status();
     }
